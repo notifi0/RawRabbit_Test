83fad034d5499f099f40ab55374b677ad0487534
Gabriel Lemire <gabriel.lemire@gmail.com>
2017-02-19 21:59:14 -0500

    Using a real class instead of string

--- RabbitMQ_Test1/Program.cs
+++ RabbitMQ_Test1/Program.cs
@@ -37,9 +37,9 @@ namespace RabbitMQ_Test1
             };
             var client = BusClientFactory.CreateDefault(config);
 
-            var subscription = client.SubscribeAsync<string>(async (msg, context) =>
+            var subscription = client.SubscribeAsync<TestClass>(async (msg, context) =>
             {
-                Console.WriteLine($"Received message: {msg}");
+                Console.WriteLine($"Received message: {msg.LastName}, {msg.FirstName} born on {msg.DateOfBirth:d}");
             }, builder =>
             {
                 builder.WithExchange(exchangeBuilder =>
@@ -50,7 +50,14 @@ namespace RabbitMQ_Test1
                 });
             });
 
-            await client.PublishAsync("Test message", Guid.NewGuid(), builder =>
+            var person1 = new TestClass
+            {
+                FirstName = "John",
+                LastName = "Smith",
+                DateOfBirth = new DateTime(1984, 03, 10)
+            };
+
+            await client.PublishAsync(person1, Guid.NewGuid(), builder =>
             {
                 builder.WithExchange(exchangeBuilder =>
                 {
@@ -62,7 +69,14 @@ namespace RabbitMQ_Test1
 
             await Task.Delay(2000);
 
-            await client.PublishAsync("Another test message!", Guid.NewGuid(), builder =>
+            var person2 = new TestClass
+            {
+                FirstName = "Melanie",
+                LastName = "Smith",
+                DateOfBirth = new DateTime(1985, 10, 25)
+            };
+
+            await client.PublishAsync(person2, Guid.NewGuid(), builder =>
             {
                 builder.WithExchange(exchangeBuilder =>
                 {
---
+++ RabbitMQ_Test1/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RabbitMQ_Test1
+{
+    public class TestClass
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
